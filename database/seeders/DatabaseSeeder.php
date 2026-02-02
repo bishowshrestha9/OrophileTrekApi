de52357750ecc710f14d779012a2cd5c4c6367b6
bishowshrestha9 <bishowg69@gmail.com>
2026-02-03 01:14:17 +0545

    Bug fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
+            'email' => 'dipshanranabhat09@gmail.com',
+            'password' => bcrypt('bpassword'),
             'role' => 'admin',
         ]);
     }
